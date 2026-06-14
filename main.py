a5a9a4ef9c635fbcf8dbd23620a0dda0852c0770
Yandapalli Tejaswini <reddytejaswini939@gmail.com>
2026-06-14 13:07:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,4 +5,10 @@ from app import app
 
 # Run the application when executed directly
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+   import os
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT",
+                                5000)),
+        debug=False
+    )
